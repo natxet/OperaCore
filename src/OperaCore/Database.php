24436b6bb1ca08422d6bce999ffb8ea056eebe1e
natxet <nacho@cofre.net>
2012-11-10 20:55:51 +0100

    You can also define a locale that will be set at the init commands in Mysql

--- src/OperaCore/Database.php
+++ src/OperaCore/Database.php
@@ -23,9 +23,16 @@ class Database
 
 		$connection = ( ( 'localhost' == $p['hostname'] ) && ( isset( $p['socket'] ) && $p['socket'] ) ) ? ';unix_socket=' . $p['socket'] : ';host=' . $p['hostname'];
 
+		$init_sets = array();
+		if(isset($g['charset'])) $init_sets[]  = "NAMES " . $g['charset'];
+		if(isset($g['locale'])) $init_sets[]  = "lc_time_names = '" . $g['locale'] . "'";
+		if(count($init_sets)) {
+			$init_commands = array( \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET ' . implode( $init_sets, ',' ) );
+		}
+		else $init_commands = NULL;
+
 		$this->profiles[$profile] = new \PDO(
-			'mysql:dbname=' . $p['database'] . $connection, $p['username'], $p['password'],
-			array( \PDO::MYSQL_ATTR_INIT_COMMAND  => "SET NAMES {$g['charset']}" )
+			'mysql:dbname=' . $p['database'] . $connection, $p['username'], $p['password'], $init_commands
 		);
 
 		$this->profiles[$profile]->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION );
