2d80f44711eab4f5b2f9b68c1b0da7ab7184c184
natxet <icofre@gmail.com>
2013-03-06 21:17:32 +0100

    BE AWARE! This is a MAJOR change, affecting current projects and migration may be needed! Now, the Environment is stored in a .env file on the root of the project (next to composer.json) and App name is passed on constructing the Boostrap. The index of the projects should be: /app/AppName/Public/www/index.php     <?php     require( '../../../../vendor/natxet/OperaCore/src/OperaCore/Bootstrap.php' );     $bootstrap  = new \OperaCore\Bootstrap( 'AppName' );     $dispatcher = new \OperaCore\Dispatcher( $bootstrap->getContainer() );
    /.env
        dev

--- src/OperaCore/Bootstrap.php
+++ src/OperaCore/Bootstrap.php
@@ -2,6 +2,12 @@
 
 namespace OperaCore;
 
+// TO-DO: when all projects are migrated, erase the "defined"
+defined( 'OPERACORE_PATH' )   or define( 'OPERACORE_PATH', __DIR__ . '/' );
+defined( 'VENDOR_PATH' )      or define( 'VENDOR_PATH', realpath( OPERACORE_PATH . '../../../../' ) . '/' );
+defined( 'BASE_PATH' )        or define( 'BASE_PATH', dirname( VENDOR_PATH ) . '/' );
+defined( 'APPS_PATH' )        or define( 'APPS_PATH', BASE_PATH . 'app/' );
+
 require( 'autoload.php' );
 
 Profile::Checkpoint( 'Begin of OperaCore' );
@@ -19,17 +25,21 @@ class Bootstrap
 	const DEVELOPMENT_ENV = 'dev'; // development environment
 
 	/**
+	 * @var String name of the app
+	 */
+	public $app;
+
+	/**
 	 * @var Container
 	 */
 	protected $container;
 
-	public function __construct()
+	public function __construct( $app )
 	{
+		$this->app       = $app;
 		$this->container = new Container();
-
 		$this->defineEnv( $this->container );
-		$this->defineApp( $this->container );
-
+		$this->defineApp();
 		$this->container->init();
 
 		Profile::Checkpoint( 'Container has been Created' );
@@ -42,13 +52,13 @@ class Bootstrap
 	 */
 	protected function getEnv( $c )
 	{
-		if( $this->isCli() )
-		{
+		$env_filename = BASE_PATH . '.env';
+		if (file_exists( $env_filename )) {
+			return trim( file_get_contents( $env_filename ) );
+		} elseif ($this->isCli()) {
 			global $argv;
 			return $argv[2];
-		}
-		else
-		{
+		} else {
 			return $c['Request']->server->get( 'APP_ENV' );
 		}
 	}
@@ -56,19 +66,10 @@ class Bootstrap
 	/**
 	 * @return string the App f.i. AcmeApp
 	 *
-	 * @param $c Container
 	 */
-	protected function getApp( $c )
+	protected function getApp()
 	{
-		if( $this->isCli() )
-		{
-			global $argv;
-			return $argv[3];
-		}
-		else
-		{
-			return $c['Request']->server->get( 'APP_APP' );
-		}
+		return $this->app;
 	}
 
 	/**
@@ -87,11 +88,11 @@ class Bootstrap
 	/**
 	 * Defines the ENV and DEBUG environment globals
 	 *
-	 * @param $c Container
+	 * @throws \UnexpectedValueException
 	 */
-	protected function defineApp( $c )
+	protected function defineApp()
 	{
-		$app = $this->getApp( $c );
+		$app = $this->getApp();
 		define( 'APP', $app );
 		define( 'APP_PATH', APPS_PATH . "$app" );
 		if( !is_dir( APP_PATH ) ) {
--- src/OperaCore/Cli/ini.php
+++ src/OperaCore/Cli/ini.php
@@ -1,42 +1,39 @@
 <?php
 
-/*
- * calls should be done like this:
- *
- */
-
 ini_set( 'error_reporting', E_ALL );
 ini_set( 'display_errors', 1 );
 
 set_error_handler( "error_handler_cli" );
 
-if ( !isCli() ) trigger_error( 'This file must be executed by php-cli' );
+if (!isCli()) {
+    trigger_error( 'This file must be executed by php-cli' );
+}
 
 date_default_timezone_set( 'Europe/Madrid' );
 
-define( 'APPS_PATH'     , realpath( __DIR__ . '/../../../../../../app' ) .'/'  );
-define( 'VENDOR_PATH'   , realpath( __DIR__ . '/../../../../../' ) .'/'  );
-define( 'OPERACORE_PATH', realpath( VENDOR_PATH . 'natxet/OperaCore/src/OperaCore/' ) .'/' );
-
-require( OPERACORE_PATH . 'Bootstrap.php' );
-
-$bootstrap  = new \OperaCore\Bootstrap();
-
 list( , $class_name, $env, $app ) = $argv;
 
-if( !isset($env) ) die("\nFATAL ERROR: The second argument for CLI should be the environment (f.i. \"dev\")\n");
-if( !isset($app) ) die("\nFATAL ERROR: The second argument for CLI should be the app (f.i. \"DemoApp\")\n");
+if (!isset( $env )) {
+    die( "\nFATAL ERROR: The second argument for CLI should be the environment (f.i. \"dev\")\n" );
+}
+if (!isset( $app )) {
+    die( "\nFATAL ERROR: The second argument for CLI should be the app (f.i. \"DemoApp\")\n" );
+}
+if (strpos( $class_name, '\\' ) === false) {
+    $class_name = "\\$app\\Cli\\$class_name";
+}
 
-if ( strpos( $class_name, '\\' ) === false ) $class_name = "\\$app\\Cli\\$class_name";
+require( __DIR__ . '/../Bootstrap.php' );
 
-$script = new $class_name( $argv, $bootstrap->getContainer() );
+$bootstrap = new \OperaCore\Bootstrap( $app );
+$script    = new $class_name( $argv, $bootstrap->getContainer() );
 
 function error_handler_cli( $errno, $errstr, $errfile, $errline )
 {
-	fwrite( STDERR, "Error $errno: $errstr in $errfile on line $errline\n" );
+    fwrite( STDERR, "Error $errno: $errstr in $errfile on line $errline\n" );
 }
 
 function isCli()
 {
-	return php_sapi_name() === "cli";
+    return php_sapi_name() === "cli";
 }
