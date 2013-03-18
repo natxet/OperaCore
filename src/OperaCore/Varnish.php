5b33f8710d560d710a786b8ace233deebe5e74dc
natxet <icofre@gmail.com>
2013-03-19 00:47:13 +0100

    using secret to get auth to ban

--- src/OperaCore/Varnish.php
+++ src/OperaCore/Varnish.php
@@ -4,14 +4,15 @@ namespace OperaCore;
 
 class Varnish
 {
+    const VARNISHADM = 'varnishadm -S /etc/varnish/secret.www-data -T 127.0.0.1:6082';
 
 	public function banUrl( $regex )
 	{
-		exec('varnishadm ban req.url ~ "' . $regex . '"');
+		exec( Varnish::VARNISHADM . ' ban req.url ~ "' . $regex . '"');
 	}
 
 	public function banHost( $regex )
 	{
-		exec('varnishadm ban req.http.host ~ "' . $regex . '"');
+		exec( Varnish::VARNISHADM . ' ban req.http.host ~ "' . $regex . '"');
 	}
 }
